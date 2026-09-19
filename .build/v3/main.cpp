f5ef41bc325234cdd49d54f2ac350d20fddf5471
DJA <jinduanxi@sina.com>
2026-09-19 23:18:21 +0800

    chore: 移除 .build/ 构建残骸并加入 gitignore

--- .build/v3/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <torch/extension.h>
-
-#include <torch/extension.h>
-void spark_v3_spfp2_forward(torch::Tensor w, torch::Tensor x,
-    torch::Tensor y, torch::Tensor b, int64_t oc, int64_t ic);
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-m.def("spark_v3_spfp2_forward", torch::wrap_pybind_function(spark_v3_spfp2_forward), "spark_v3_spfp2_forward");
-}
